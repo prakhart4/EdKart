ab70285ad8c561e80d70a731c893557005ebc7d0
Prakhar <prakhart4@gmail.com>
2023-08-26 12:20:55 +0530

    fix: db uri check | dotenv-cli added in dev

--- packages/db/src/lib/dbConnect.ts
+++ packages/db/src/lib/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env["MONGODB_URI"]!;
+const MONGODB_URI = process.env["MONGODB_URI"];
 
 type globalWithMongo = typeof globalThis & {
   mongoose?: {
@@ -8,6 +8,12 @@ type globalWithMongo = typeof globalThis & {
     promise?: Promise<typeof mongoose> | null;
   };
 };
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
